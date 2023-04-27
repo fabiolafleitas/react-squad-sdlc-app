3267b7f437f48913e6a45134e953386a5849d87b
Fabiola <fabiolaf25@gmail.com>
2023-04-27 10:37:20 -0400

    feat: :tada: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+}
